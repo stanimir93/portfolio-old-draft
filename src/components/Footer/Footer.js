22af3458858de648e6fffbf105d6be4360f4d689
Your Name <stanimir.nikolov123@gmail.com>
2022-03-22 20:22:11 +0000

    Header and footer links added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,33 @@
 import './footer.css';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
-    return <footer></footer>;
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/projects'>Projects</Link>
+                    </li>
+                    <li>
+                        <Link to='/cv'>CV</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <ul>
+                <li>
+                    <i class='bi bi-linkedin'></i>
+                </li>
+                <li>
+                    <i class='bi bi-github'></i>
+                </li>
+            </ul>
+        </footer>
+    );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import './header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
@@ -7,20 +8,28 @@ export default function Header() {
             <nav>
                 <ul>
                     <li>
-                        <i className='bi bi-house'></i>
-                        <span>Home</span>
+                        <Link to='/'>
+                            <i className='bi bi-house'></i>
+                            <span>Home</span>
+                        </Link>
                     </li>
                     <li>
-                        <i class='bi bi-code-square'></i>
-                        <span>Projects</span>
+                        <Link to='/projects'>
+                            <i class='bi bi-code-square'></i>
+                            <span>Projects</span>
+                        </Link>
                     </li>
                     <li>
-                        <i class='bi bi-file-person'></i>
-                        <span>CV</span>
+                        <Link to='/cv'>
+                            <i class='bi bi-file-person'></i>
+                            <span>CV</span>
+                        </Link>
                     </li>
                     <li>
-                        <i class='bi bi-envelope'></i>
-                        <span>Contact</span>
+                        <Link to='/contact'>
+                            <i class='bi bi-envelope'></i>
+                            <span>Contact</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
