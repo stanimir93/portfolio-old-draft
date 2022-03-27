d30d3fea55123f3b512c9e3fbc2f6fe6b8149549
Your Name <stanimir.nikolov123@gmail.com>
2022-03-27 14:35:31 +0100

    Footer & Header Layout Progress

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,36 +3,54 @@ import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className='p-8 '>
             <nav>
-                <ul>
+                <ul className='flex place-content-center text-base mb-6'>
                     <li>
-                        <Link to='/'>Home</Link>
+                        <Link className='mx-1 p-1' to='/'>
+                            Home
+                        </Link>
                     </li>
                     <li>
-                        <Link to='/projects'>Projects</Link>
+                        <Link className='mx-1 p-1' to='/projects'>
+                            Projects
+                        </Link>
                     </li>
                     <li>
-                        <Link to='/cv'>CV</Link>
+                        <Link className='mx-1 p-1' to='/cv'>
+                            CV
+                        </Link>
                     </li>
                     <li>
-                        <Link to='/contact'>Contact</Link>
+                        <Link className='mx-1 p-1' to='/contact'>
+                            Contact
+                        </Link>
                     </li>
                 </ul>
             </nav>
-            <ul>
+            <ul className='flex place-content-center text-xl'>
                 <li>
-                    <a href='https://github.com/stanimir93' target='_blank' title='GitHub'>
+                    <a className='mx-2 p-1' href='https://github.com/stanimir93' target='_blank' title='GitHub'>
                         <i className='bi bi-github'></i>
                     </a>
                 </li>
                 <li>
-                    <a href='https://www.linkedin.com/in/stanimir-nikolov/' target='_blank' title='LinkedIn'>
+                    <a
+                        className='mx-2 p-1'
+                        href='https://www.linkedin.com/in/stanimir-nikolov/'
+                        target='_blank'
+                        title='LinkedIn'
+                    >
                         <i className='bi bi-linkedin'></i>
                     </a>
                 </li>
                 <li>
-                    <a href='mailto:contact@stanimirnikolov.com' target='_blank' title='contact@stanimirnikolov.com'>
+                    <a
+                        className='mx-2 p-1'
+                        href='mailto:contact@stanimirnikolov.com'
+                        target='_blank'
+                        title='contact@stanimirnikolov.com'
+                    >
                         <i className='bi bi-envelope-fill'></i>
                     </a>
                 </li>
